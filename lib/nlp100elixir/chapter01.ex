4c15c066f1030aba31dd997d5292c18c28ddd4fe
root <root@www6095ue.sakura.ne.jp>
2016-09-15 04:57:40 +0900

    chapter01 ex00 add

---
+++ lib/nlp100elixir/chapter01.ex
@@ -0,0 +1,7 @@
+defmodule Nlp100elixir.Chapter01 do
+  def ex00 do
+    "stressed"
+    |> String.reverse
+    |> to_string
+  end
+end
---
+++ test/chapter01_test.exs
@@ -0,0 +1,8 @@
+defmodule Chapter01Test do
+  use ExUnit.Case
+  doctest Nlp100elixir.Chapter01
+
+  test "ex00" do
+    assert "desserts" == Nlp100elixir.Chapter01.ex00
+  end
+end
