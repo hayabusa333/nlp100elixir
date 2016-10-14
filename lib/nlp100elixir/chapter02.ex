d4a496f1cb68958be93848d212be85770743ead6
hayabusa333 <junnichi0121@gmail.com>
2016-10-14 21:11:08 +0900

    add ex12

--- lib/nlp100elixir/chapter02.ex
+++ lib/nlp100elixir/chapter02.ex
@@ -14,4 +14,33 @@ defmodule Nlp100elixir.Chapter02 do
     {:ok, contents} = File.read "hightemp.txt"
     String.replace(contents,"\t"," ")
   end
+
+  def ex12 do
+    {:ok, contents} = File.read "hightemp.txt"
+    {:ok, col1} = File.open "col1.txt", [:write]
+    {:ok, col2} = File.open "col2.txt", [:write]
+    split_file = String.split(contents, "\n")
+    split_count = Enum.count(split_file)
+
+    split_file
+    |> Enum.with_index
+    |> Enum.each( (fn(x) ->
+      if (elem(x, 1) != split_count and elem(x, 0) != "") do
+        IO.binwrite col1, Enum.at(String.split(elem(x, 0), "\t"), 0)
+        IO.binwrite col1, "\n" 
+      end
+    end) )
+
+    split_file
+    |> Enum.with_index
+    |> Enum.each((fn(x) -> 
+      if (elem(x, 1) != split_count and elem(x, 0) != "") and Enum.at(String.split(elem(x, 0), "\t"), 1) != nil do
+        IO.binwrite col2, Enum.at(String.split(elem(x, 0), "\t"), 1)
+        IO.binwrite col2, "\n"  
+      end
+    end) )
+
+   File.close col1
+   File.close col2
+  end
 end
